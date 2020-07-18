d06c6bcabce41136b7cc4a260fb2362cf0699dba
noahlincke <noah@lincke.org>
2020-07-18 14:18:17 -0700

    Initial commit

---
+++ Carve/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Carve
+//
+//  Created by Noah Lincke on 7/18/20.
+//  Copyright © 2020 umich2020. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Carve")
+            .multilineTextAlignment(.center)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
