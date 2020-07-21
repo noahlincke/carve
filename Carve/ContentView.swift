d7cc1b8d774d839af43ef93f47e338982f22f7d7
elizabethyangg <ee.yang70@gmail.com>
2020-07-21 13:46:52 -0700

    firebase and uploading stuff

--- Carve/ContentView.swift
+++ Carve/ContentView.swift
@@ -7,11 +7,38 @@
 //
 
 import SwiftUI
+import Combine
+import FirebaseStorage
+
+let FILE_NAME = "images/imageFileTest.jpg"
 
 struct ContentView: View {
+    @State var shown = false
+    @State var imageURL = ""
     var body: some View {
-        Text("Carve")
-            .multilineTextAlignment(.center)
+        VStack {
+            if imageURL != "" {
+                FirebaseImageView(imageURL: imageURL)
+            }
+            
+            Button(action: { self.shown.toggle() }) {
+                Text("Upload Image").font(.title).bold()
+            }.sheet(isPresented: $shown) {
+                imagePicker(shown: self.$shown,imageURL: self.$imageURL)
+                }.padding(10).background(Color.purple).foregroundColor(Color.white).cornerRadius(20)
+        }.onAppear(perform: loadImageFromFirebase).animation(.spring())
+    }
+    
+    func loadImageFromFirebase() {
+        let storage = Storage.storage().reference(withPath: FILE_NAME)
+        storage.downloadURL { (url, error) in
+            if error != nil {
+                print((error?.localizedDescription)!)
+                return
+            }
+            print("Download success")
+            self.imageURL = "\(url!)"
+        }
     }
 }
 
---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Carve' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Carve
+# add the Firebase pod for Google Analytics
+pod 'Firebase/Analytics'
+# add pods for any other desired Firebase products
+# https://firebase.google.com/docs/ios/setup#available-pods
+pod 'Firebase/Storage'
+
+  target 'CarveTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'CarveUITests' do
+    # Pods for testing
+  end
+
+end
